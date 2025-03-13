74e3945035f32f082d7bd74a8f98d52f85433398
Petter Melander <petter.melander@nordaxon.com>
2025-03-13 13:59:54 +0100

    update inference

--- src/inference.py
+++ src/inference.py
@@ -1,10 +1,28 @@
-import random
+import os
+
+import cv2
+from ultralytics import YOLO
+
 from src.logger import setup_logging
 
 logger = setup_logging("inference_service")
+model = YOLO(os.getenv("MODEL_PATH")).to(os.getenv("INFERENCE_DEVICE"))
+img_size = int(os.getenv("IMAGE_SIZE"))
 
 
 def run_inference() -> bool:
-    result = random.choice([True, False])
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, img_size)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, img_size)
+    if cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            raise Exception("Could not read frame")
+        result = model.predict(frame, imgsz=img_size, verbose=True)[0]
+        logger.debug(result.verbose())
+        logger.debug(result.speed)
+        result = result.probs.top1 == 0
+    else:
+        raise Exception("Could not open video capture")
     logger.debug(f"Inference result: {result}")
     return result
--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,31 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
+
+import cv2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CV2_LOG_LEVELS = {
+    "VERBOSE": 6,
+    "DEBUG": 5,
+    "INFO": 4,
+    "WARN": 3,
+    "WARNING": 3,
+    "ERROR": 2,
+    "FATAL": 1,
+    "CRITICAL": 1,
+    "SILENT": 0,
+    "NOTSET": 0,
+}
 
 
 def setup_logging(name: str):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+    logger.setLevel(log_level)
+    cv2.setLogLevel(CV2_LOG_LEVELS.get(log_level))
 
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
